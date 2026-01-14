ed68a079f11ea17747688997d130e4c9c6495244
mrouton23 <46700595+mrouton23@users.noreply.github.com>
2026-01-13 19:14:56 -0800

    Through 3.16. Does not allow adding a person that already exists.

--- index.js
+++ index.js
@@ -1,54 +1,33 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
-const morgan = require('morgan')
 const Person = require('./models/person')
+const morgan = require('morgan')
 
-app.use(express.json())
+const app = express()
+
 app.use(express.static('dist'))
+app.use(express.json())
+
 morgan.token('body', (request) => {
     return JSON.stringify(request.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-    const numPersons = persons.length
-    const dateTime = Date(Date.now()).toString()
-    
-    response.send(
-        `
-        <h1>Phonebook has info for ${persons.length} persons </h1> 
-        <h1>Request received at ${dateTime} </h1>
-        `
-    )
+    Person.find({}).then(persons => {
+        const dateTime = Date(Date.now()).toString()
+        response.send(
+            `
+            <h1>Phonebook has info for ${persons.length} persons </h1> 
+            <h1>Request received at ${dateTime} </h1>
+            `
+        )
+    })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -57,33 +36,27 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }    
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
-    
-    // const id = request.params.id
-    // const person = persons.find(person => person.id === id)
-  
-    // if (person) {    
-    //     response.json(person)
-    // } else {
-    //     response.status(404).end()  
-    // }
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
-
-// const generateId = () => {
-//     return String(Math.floor(Math.random() * 100000))
-// }
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
     if (!body.name) {
@@ -98,21 +71,51 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (undefined = persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: `${body.name} already exists in the phonebook`
-       })
-    }
-  
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+    Person.find({name: body.name})
+        .then(person => {
+            if(person){
+                console.log(`We already have a person in the database by ${body.name}`)
+                response.status(400).end()
+            } else {
+                const person = new Person({
+                    name: body.name,
+                    number: body.number,
+                })
+                person.save().then(savedPerson => {
+                    response.json(savedPerson)
+                })
+            }
+        })
+        .catch(error => next(error))
+    
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    // const person = new Person({
+    //     name: body.name,
+    //     number: body.number,
+    // })
+
+    // person.save().then(savedPerson => {
+    //     response.json(savedPerson)
+    // })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
